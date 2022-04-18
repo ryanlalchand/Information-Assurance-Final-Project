fe9855b7bf48d14424b87c0a72f0b5088a2588a6
Ryan Lalchand <70449616+ryanlalchand@users.noreply.github.com>
2022-04-18 16:48:38 -0400

    added outline for part 2

---
+++ Part_2/key_gen.py
@@ -0,0 +1,9 @@
+# Part 2: Asymmetric key encryption using python
+
+# Import the rsa library
+# Use the rsa library to generate a key pair and save them
+# Make a copy of the key from part 1 and place it in a file called rsa_sym_key_plain.txt
+# Encrypt that file using the rsa key pair you generated. Place the encrypted version into a file called rsa_sym_key_cipher.txt
+# Test it and make sure that you can decrypt it back to plain text.
+
+https://stuvel.eu/python-rsa-doc/usage.html
