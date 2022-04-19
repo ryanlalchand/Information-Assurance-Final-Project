5ed33dfe1540bb7ea259760eaf3c18d53637cc8d
Kelly Lam <klam@student.ysu.edu>
2022-04-19 12:44:29 -0400

    wrote part 2

--- Part_2/key_gen.py
+++ Part_2/key_gen.py
@@ -1,9 +1,30 @@
 # Part 2: Asymmetric key encryption using python
+# https://stuvel.eu/python-rsa-doc/usage.html
 
 # Import the rsa library
-# Use the rsa library to generate a key pair and save them
-# Make a copy of the key from part 1 and place it in a file called rsa_sym_key_plain.txt
-# Encrypt that file using the rsa key pair you generated. Place the encrypted version into a file called rsa_sym_key_cipher.txt
-# Test it and make sure that you can decrypt it back to plain text.
+import rsa
 
-https://stuvel.eu/python-rsa-doc/usage.html
+# Use the rsa library to generate a key pair and save them
+(pubKey, privKey) = rsa.newkeys(512)
+
+# Make a copy of the key from part 1
+keyFile = open("./Part_1/key.key", "rb")
+pt1Key = keyFile.read()
+keyFile.close()
+
+# Place it in a file called rsa_sym_key_plain.txt
+plainFile = open("./Part_2/rsa_sym_key_plain.txt", "wb")
+plainFile.write(pt1Key)
+plainFile.close()
+
+# Encrypt that file using the rsa key pair you generated.
+crypto = rsa.encrypt(pt1Key, pubKey)
+
+# Place the encrypted version into a file called rsa_sym_key_cipher.txt
+cipherFile = open("./Part_2/rsa_sym_key_cipher.txt", "wb")
+cipherFile.write(crypto)
+cipherFile.close()
+
+# Test it and make sure that you can decrypt it back to plain text.
+message = rsa.decrypt(crypto, privKey)
+print(message)
